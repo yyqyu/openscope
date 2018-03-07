276fee58d6132634f6e824a39d17dc7a31f0df3e
Eric <indianbhaji@gmail.com>
2018-03-07 13:32:36 +0000

    refactor/241 - `wip: false`

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -111,7 +111,7 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
             level: 'easy',
             name: 'London Luton Airport',
             premium: false,
-            wip: true
+            wip: false
         },
         // {
         //     icao: 'egkk',
